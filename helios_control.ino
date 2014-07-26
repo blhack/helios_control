394829907d1fd3968e93d4694ef0b93ba58417ea
blhack <ryan@Ryans-MacBook-Pro.local>
2014-07-26 00:07:43 -0700

    Now working with dmxsimple and midi

--- helios_control.ino
+++ helios_control.ino
@@ -1,88 +1,239 @@
-// The checkMIDI function of this program is based heavily on one by By Amanda Ghassaei
-// linked here: http://www.instructables.com/id/Send-and-Receive-MIDI-with-Arduino/
+#include <DmxSimple.h>
+#include <MIDI.h>
 
-/* * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
+#define max_channels 512
+#define space 61
+#define max_keys 121
+#define num_flashes 8
 
-*/
+byte channels[max_channels];
+int reds[] = {474};
+int greens[] = {475};
+int blues[] = {476};
 
+bool keys[max_keys];
+bool flashes[num_flashes];
 
-byte commandByte;
-byte noteByte;
-byte velocityByte;
+bool flashOn = false;
 
-byte noteOn = 144;
-byte noteOff = 128;
+byte displayed_r = 0;
+byte displayed_g = 0;
+byte displayed_b = 0;
 
-int torches[3] = {0,0,0};
+byte measured_r = 0;
+byte measured_g = 0;
+byte measured_b = 0;
 
-int max_time_on = 5000; //the maximum time, in MS, that a torch can stay on
+byte b_max = space;
+byte g_max = (space - (space/3.0));
+byte r_max = (space - ((space/3.0)*2));
 
-void setup(){
-  Serial.begin(31250);
-  pinMode(13,OUTPUT);
-  digitalWrite(13,LOW);
+MIDI_CREATE_DEFAULT_INSTANCE();
+
+void setup() {
+  DmxSimple.usePin(4);
+  DmxSimple.maxChannel(max_channels);
+
+  all_on();
+  delay(500);
+  all_off();
+
+  MIDI.setHandleNoteOn(handleNoteOn);  // Put only the name of the function
+  MIDI.setHandleNoteOff(handleNoteOff);
+  MIDI.begin(MIDI_CHANNEL_OMNI);
 }
 
-void checkMIDI() {
-  do {
-    if (Serial.available()) {
-      commandByte = Serial.read();
-      noteByte = Serial.read();
-      velocityByte = Serial.read();
-      
-      if (commandByte == noteOn) {
-        switch(noteByte) {
-          case 60:
-            torches[0] = max_time_on;
-            break;
-          case 50:
-            torches[1] = max_time_on;
-            break;
-        }
-      }
-      
-      if (commandByte == noteOff) {
-        switch(noteByte) {
-          case 60:
-            torches[0] = 0;
-            break;
-          case 50:
-            torches[1] = 0;
-            break;
-        }
-      }
+void tick() {
+  if (measured_r > displayed_r) {
+    displayed_r++;
+  }
+  if (measured_r < displayed_r) {
+    displayed_r--;
+  }
+
+  if (measured_g > displayed_g) {
+    displayed_g++;
+  }
+  if (measured_g < displayed_g) {
+    displayed_g--;
+  }
+
+  if (measured_b > displayed_b) {
+    displayed_r++;
+  }
+  if (measured_b < displayed_b) {
+    displayed_b--;
+  }
+
+  for (int i=0; i<(sizeof(reds)/sizeof(int)); i++) {
+    DmxSimple.write(reds[i], displayed_r);
+  }
+  for (int i=0; i<(sizeof(greens)/sizeof(int)); i++) {
+    DmxSimple.write(greens[i], displayed_g);
+  }
+  for (int i=0; i<(sizeof(blues)/sizeof(int)); i++) {
+    DmxSimple.write(blues[i], displayed_g);
+  }
+}
+
+void rgb(int pitch) {
+  measured_r = 0;
+  measured_g = 0;
+  measured_b = 0;
+
+
+  if (pitch > 60) {
+    int offset = (pitch % 60);
+    pitch = 60 - offset;
+  }
+
+  if (pitch > space) {
+    pitch = pitch - (pitch - space);
+  }
+
+  if (pitch < r_max) {
+    measured_r = map(pitch, 0, r_max, 0, 255);
+  }
+  if ((pitch >= r_max) && (pitch <= g_max)) {
+    measured_b = map(pitch - r_max, 0, r_max, 0, 255);
+    measured_r = 255 - measured_b;
+  }
+  if (pitch >= g_max) {
+    measured_r = map(pitch - g_max, 0, r_max, 0, 255);
+    measured_g = 255 - measured_r;
+  }
+  if (pitch == space) {
+    measured_b = 255;
+  }
+}
+
+float getpitch() {
+  float total = 0;
+  float oncount = 0;
+  for (int i=0; i<(sizeof(keys)/sizeof(bool)); i++) {
+    if (keys[i] == true) {
+      total = total + i;
+      oncount++;
     }
-  } while (Serial.available() > 2);//when at least three bytes available
+  }
+  return(total/oncount);
 }
 
-void torch_on(int torch) {
-  //turn on the torch at address $foo
-}
-
-void torch_off(int torch) {
-  //turn off the torch ad address $f00
-}
-
-void expire_torches() {
+void flash() {
+  if (flashes[0] == true) {
+    DmxSimple.write(474, 255);
+  }
+  else {
+    DmxSimple.write(474,0);
+  }
   
-  //use an event loop to turn torches off after either the note has been released, or a timer of 5 seconds expires.
+  if (flashes[1] == true) {
+    //
+  }
+  else {
+    //
+  }
   
-  for (int i = 0; i < 3; i++) {
-    if (torches[i] == max_time_on) {
-      //this torch has just been triggered.  Turn it on
-    }
-    if (torches[i] == 0) {
-      //this torch has either expired or its key has been released; turn the torch off
-    }
-    torches[i] = torches[i] - 1;
+  if (flashes[2] == true) {
+    //  
+  }
+  else {
+    //  
+  }
+  
+  if (flashes[3] == true) {
+    
+  }
+  else {
+    
+  }
+  
+  if (flashes[4] == true) {
+    //  
+  }
+  else {
+    //  
+  }
+  
+  if (flashes[5] == true) {
+    //  
+  }
+  else {
+    //  
+  }
+  
+  if (flashes[6] == true) {
+    //  
+  }
+  else {
+    //  
+  }
+  
+  if (flashes[7] == true) {
+    //  
+  }
+  else {
+    //  
+  }
+}
+
+void handleNoteOn(byte channel, byte pitch, byte velocity) {
+  if (channel == 10) {
+    flashes[pitch-36] = true;
+    flash();
+  }
+  else {
+    keys[pitch] = true;
+    pitch = getpitch();
+    rgb(pitch);
+  }
+}
+
+void handleNoteOff(byte channel, byte pitch, byte velocity) {
+  if (channel == 10) {
+    flashes[pitch-36] = false;
+    flash();
+  }
+  else {
+    keys[pitch] = false;
+    pitch = getpitch();
+    rgb(pitch);
+  }
+}
+
+void all_on() {
+  for (int i=0; i<(sizeof(reds)/sizeof(int)); i++) {
+    DmxSimple.write(reds[i],255);
+  }
+  delay(500);
+  for (int i=0; i<(sizeof(greens)/sizeof(int)); i++) {
+    DmxSimple.write(greens[i],255);
+  }
+  delay(500);
+  for (int i=0; i<(sizeof(blues)/sizeof(int)); i++) {
+    DmxSimple.write(blues[i],255);
+  }
+}
+
+void all_off() {
+  for (int i=0; i<(sizeof(reds)/sizeof(int)); i++) {
+    DmxSimple.write(reds[i],0);
+  }
+  delay(500);
+  for (int i=0; i<(sizeof(greens)/sizeof(int)); i++) {
+    DmxSimple.write(greens[i],0);
+  }
+  delay(500);
+  for (int i=0; i<(sizeof(blues)/sizeof(int)); i++) {
+    DmxSimple.write(blues[i],0);
   }
 }
 
 void loop() {
-  checkMIDI();
-  write_torches();
-  delay(1);
+  if (((millis() % 5) == 0)) {
+    int pitch = getpitch();
+    rgb(pitch);
+    tick();
+  }
+  MIDI.read();
 }
