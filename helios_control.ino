fc72d6527bc1b86b60e1d81416ec1c391ec66971
blhack <ryan@Ryans-MacBook-Pro.local>
2014-07-26 17:36:22 -0700

    Much better color mixing via the rgb() function

--- helios_control.ino
+++ helios_control.ino
@@ -1,28 +1,30 @@
 #include <DmxSimple.h>
 #include <MIDI.h>
 
-#define max_channels 512
-#define space 61
-#define max_keys 121
-#define num_flashes 8
+#define max_channels 512     //the maximum number of DMX channels to support
+#define space 61.0           //the number of available hardware keys that is available at a time (must be a float)
+#define max_keys 121         //the number of keys on your keyboard 121 is the highest pitch that will ever be received
+#define num_flashes 8        //the number of buttons that can be associated with a "flash" or "torch" event
+#define min_key 36           //the leftmost midi key ID when transpose is 0
+#define max_key 96           //the rightmost midi key ID when transpose is 0
 
 byte channels[max_channels];
-int reds[] = {474};
-int greens[] = {475};
-int blues[] = {476};
+int reds[] = {474};          //red DMX channels
+int greens[] = {1};        //green DMX channels
+int blues[] = {476};         //blue DMX channels
 
-bool keys[max_keys];
-bool flashes[num_flashes];
+bool keys[max_keys];         //an array that stores key states
+bool flashes[num_flashes];   //an array that stores "flash" or "torch" states 
 
-bool flashOn = false;
+bool flashOn = false;        //is "flash" enabled?  (This will not progress color changes)
 
-byte displayed_r = 0;
-byte displayed_g = 0;
-byte displayed_b = 0;
+byte displayed_r = 0;        //the currently displayed R value
+byte displayed_g = 0;        //the currently displayed G value
+byte displayed_b = 0;        //the currently displayed B value
 
-byte measured_r = 0;
-byte measured_g = 0;
-byte measured_b = 0;
+byte measured_r = 0;         //the currently *measured* R value (as determined by key presses)
+byte measured_g = 0;         //the currently *measured* G Value (as determined by key presses)
+byte measured_b = 0;         //the currently *measured* B Value (as determined by key presses)
 
 byte b_max = space;
 byte g_max = (space - (space/3.0));
@@ -41,6 +43,7 @@ void setup() {
   MIDI.setHandleNoteOn(handleNoteOn);  // Put only the name of the function
   MIDI.setHandleNoteOff(handleNoteOff);
   MIDI.begin(MIDI_CHANNEL_OMNI);
+
 }
 
 void tick() {
@@ -59,7 +62,7 @@ void tick() {
   }
 
   if (measured_b > displayed_b) {
-    displayed_r++;
+    displayed_b++;
   }
   if (measured_b < displayed_b) {
     displayed_b--;
@@ -72,7 +75,7 @@ void tick() {
     DmxSimple.write(greens[i], displayed_g);
   }
   for (int i=0; i<(sizeof(blues)/sizeof(int)); i++) {
-    DmxSimple.write(blues[i], displayed_g);
+    DmxSimple.write(blues[i], displayed_b);
   }
 }
 
@@ -81,30 +84,28 @@ void rgb(int pitch) {
   measured_g = 0;
   measured_b = 0;
 
-
-  if (pitch > 60) {
-    int offset = (pitch % 60);
-    pitch = 60 - offset;
+  if (pitch == 0) {
+    return;
+  }
+  
+  if (pitch > max_key) {
+    int offset = (pitch - max_key);
+    pitch = max_key - offset;
+  }
+  
+  if (pitch < min_key) {
+    int offset = (min_key - pitch);
+    pitch = min_key + offset;
   }
 
-  if (pitch > space) {
-    pitch = pitch - (pitch - space);
+  measured_r = map(pitch,min_key,max_key,0,255);
+  measured_g = map(pitch,min_key,max_key,255,0);
+  
+  if (pitch > (min_key + (space/2.0))) {
+    int offset = (pitch - (min_key + (space/2.0)));
+    pitch = (min_key + (space/2.0)) - offset;
   }
-
-  if (pitch < r_max) {
-    measured_r = map(pitch, 0, r_max, 0, 255);
-  }
-  if ((pitch >= r_max) && (pitch <= g_max)) {
-    measured_b = map(pitch - r_max, 0, r_max, 0, 255);
-    measured_r = 255 - measured_b;
-  }
-  if (pitch >= g_max) {
-    measured_r = map(pitch - g_max, 0, r_max, 0, 255);
-    measured_g = 255 - measured_r;
-  }
-  if (pitch == space) {
-    measured_b = 255;
-  }
+  measured_b = map(pitch,min_key,(min_key + (space/2.0)),0,255);
 }
 
 float getpitch() {
@@ -119,68 +120,10 @@ float getpitch() {
   return(total/oncount);
 }
 
-void flash() {
-  if (flashes[0] == true) {
-    DmxSimple.write(474, 255);
-  }
-  else {
-    DmxSimple.write(474,0);
-  }
-  
-  if (flashes[1] == true) {
-    //
-  }
-  else {
-    //
-  }
-  
-  if (flashes[2] == true) {
-    //  
-  }
-  else {
-    //  
-  }
-  
-  if (flashes[3] == true) {
-    
-  }
-  else {
-    
-  }
-  
-  if (flashes[4] == true) {
-    //  
-  }
-  else {
-    //  
-  }
-  
-  if (flashes[5] == true) {
-    //  
-  }
-  else {
-    //  
-  }
-  
-  if (flashes[6] == true) {
-    //  
-  }
-  else {
-    //  
-  }
-  
-  if (flashes[7] == true) {
-    //  
-  }
-  else {
-    //  
-  }
-}
 
 void handleNoteOn(byte channel, byte pitch, byte velocity) {
   if (channel == 10) {
-    flashes[pitch-36] = true;
-    flash();
+    torch_on(pitch);
   }
   else {
     keys[pitch] = true;
@@ -191,13 +134,34 @@ void handleNoteOn(byte channel, byte pitch, byte velocity) {
 
 void handleNoteOff(byte channel, byte pitch, byte velocity) {
   if (channel == 10) {
-    flashes[pitch-36] = false;
-    flash();
+    torch_off(pitch);
   }
   else {
     keys[pitch] = false;
     pitch = getpitch();
     rgb(pitch);
+  }
+}
+
+void torch_on(int pitch) {
+  switch(pitch) {
+    case 36:
+      DmxSimple.write(475, 255);
+      break;
+    case 42:
+      DmxSimple.write(475, 255);
+      break;
+  }
+}
+
+void torch_off(int pitch) {
+  switch(pitch) {
+    case 36:
+      DmxSimple.write(475, 0);
+      break;
+    case 42:
+      DmxSimple.write(475, 0);
+      break;
   }
 }
 
